412fe945d2b1d400f379ed1924bf79225a9eab46
Peter Boyle <31522669+CaptainCrossbar@users.noreply.github.com>
2021-05-24 12:16:08 -0400

    Create helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("\n\n\t\tC Study Session - We Gonna learn today\n\n\n");
+  printf("Hello World!!! We are just getting started. Gonna learn all the tings\n\n\n");
+  printf("\t\tCoding is the duality of man!!!\n\n");
+  printf("\t\tIt is both:");
+  printf("\t\tGood and Evil");
+  printf("\t\tFun and Frustrating");
+  return 0;
+}
+  
