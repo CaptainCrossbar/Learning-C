048202ebc7560f16cadfdd9ceb82047b0065f190
Peter Boyle <31522669+CaptainCrossbar@users.noreply.github.com>
2021-05-24 12:39:10 -0400

    Create ASCIIcharValue.c

---
+++ ASCIIcharValue.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("\n\n\t\tC Study Session - We Gonna learn today\n\n\n");
+  
+  // Character value
+  char c;
+  
+  // Prompts the user for input, wants a character input
+  printf("Please enter in a character : ");
+  scanf("%c", &c);
+  printf("\n\nThe ASCII value of %c = %d",c,c);
+  
+  printf("\t\tCoding is the duality of man!!!\n\n");
+  printf("\t\tIt is both:");
+  printf("\t\tGood and Evil");
+  printf("\t\tFun and Frustrating");
+  return 0;
+}
