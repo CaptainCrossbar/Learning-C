84cb911e23fc137a2915748c12b6371f21636456
Peter Boyle <31522669+CaptainCrossbar@users.noreply.github.com>
2021-05-25 09:09:53 -0400

    Update usingGetsFunction.c

--- usingGetsFunction.c
+++ usingGetsFunction.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*
+Below is a program that demostrates the gets function
+ - gets() takes only a single line at a time i.e all the words before hitting \n(enter key).
+ 
+Some of the important points about scanf() and gets() are:
+ - scanf() and gets() both are used to take input from the user.
+ - scanf() can only take input until it encounters a space. The words after space are ignored by it.
+ - gets() is used to take a single input at a time but can be used to input a complete sentence with spaces unlike scanf().
+*/
+
 int main()
 {
   printf("\n\n\t\tC Study Session - We Gonna learn today\n\n\n");
